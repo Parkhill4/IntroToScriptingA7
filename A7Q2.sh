c83d48b9471a0fb62051011351e214df07c5a01e
Parkhill4 <98786222+Parkhill4@users.noreply.github.com>
2022-04-06 13:48:32 -0500

    Add files via upload

---
+++ A7Q2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+name="Ethan Parkhill"
+class1="Intro to Scripting"
+class2="Data Structures"
+class3="Network Systems"
+echo "My name is $name and I am in 3 classes, including $class1, $class2, $class3"
